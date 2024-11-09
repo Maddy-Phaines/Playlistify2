635c83fb8f29c5790b60b842b649a728ab727d53
Maddy-Phaines <maddy.phaines@gmail.com>
2024-11-09 15:14:48 +0000

    Add ArtistProfile component basic

--- src/components/ArtistProfile/ArtistProfile.tsx
+++ src/components/ArtistProfile/ArtistProfile.tsx
@@ -1,4 +1,16 @@
 import React from "react";
 import styles from "./ArtistProfile.module.css";
 
-const ArtistProfile;
+const ArtistProfile: React.FC<{ artist: string; cover: string }> = ({
+  artist,
+  cover,
+}) => {
+  return (
+    <div className={styles.profile}>
+      <img src={cover} alt={artist} />
+      <h2 className={styles.artistName}>{artist}</h2>
+    </div>
+  );
+};
+
+export default ArtistProfile;
